cd651176036b4be1ac433e2b07c83c116918ad22
Alex Beloi <a.beloi@samsung.com>
2016-06-23 11:19:57 -0700

    add: ISSampler class for importance sampling

---
+++ contrib/vpgis_cartpole.py
@@ -0,0 +1,35 @@
+from rllab.algos.vpg import VPG
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.envs.box2d.cartpole_env import CartpoleEnv
+from rllab.envs.normalized_env import normalize
+from rllab.policies.gaussian_mlp_policy import GaussianMLPPolicy
+from contrib.is_sampler import ISSampler
+
+"""
+Example using VPG with ISSampler, iterations alternate between live and
+importance sampled iterations.
+"""
+
+env = normalize(CartpoleEnv())
+
+policy = GaussianMLPPolicy(
+    env_spec=env.spec,
+    # The neural network policy should have two hidden layers, each with 32 hidden units.
+    hidden_sizes=(32, 32)
+)
+
+baseline = LinearFeatureBaseline(env_spec=env.spec)
+
+algo = VPG(
+    env=env,
+    policy=policy,
+    baseline=baseline,
+    batch_size=4000,
+    max_path_length=100,
+    n_itr=40,
+    discount=0.99,
+    step_size=0.01,
+    sampler_cls=ISSampler,
+    sampler_args=dict(n_backtrack=1),
+)
+algo.train()
