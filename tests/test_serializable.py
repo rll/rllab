82a1499783ede1f2a0894de524df92a931a86e77
haarnoja <haarnoja@users.noreply.github.com>
2017-07-18 17:37:22 +0100

    Fix bug with positional arguments in Serializable. (#173)

--- tests/test_serializable.py
+++ tests/test_serializable.py
@@ -14,12 +14,26 @@ class Simple(Parameterized, Serializable):
         return [self.w]
 
 
+class AllArgs(Serializable):
+    def __init__(self, vararg, *args, **kwargs):
+        Serializable.quick_init(self, locals())
+        self.vararg = vararg
+        self.args = args
+        self.kwargs = kwargs
+
+
 def test_serializable():
     with suppress_params_loading():
         obj = Simple(name="obj")
         obj1 = Serializable.clone(obj, name="obj1")
         assert obj.w.name.startswith('obj/')
         assert obj1.w.name.startswith('obj1/')
+
+        obj2 = AllArgs(0, *(1,), **{'kwarg': 2})
+        obj3 = Serializable.clone(obj2)
+        assert obj3.vararg == 0
+        assert len(obj3.args) == 1 and obj3.args[0] == 1
+        assert len(obj3.kwargs) == 1 and obj3.kwargs['kwarg'] == 2
 
 
 if __name__ == "__main__":
