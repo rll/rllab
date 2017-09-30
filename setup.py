ca0a8b43da74a6e2aa8211b6014b5e644d021628
Daniel <daniellsmarta@gmail.com>
2017-09-30 18:28:26 +0100

    setup_fix

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 
 setup(
     name='rllab',
+    packages=[package for package in find_packages()
+                if package.startswith('rllab')],
     version='0.1.0',
-    packages=['rllab'],
 )
