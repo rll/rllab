a23f7689160ce34c6afd729768623d14fbc6a343
tzattack <tzattack@gmail.com>
2017-10-09 11:39:49 +0800

    test

--- examples/dqn.py
+++ examples/dqn.py
@@ -5,3 +5,36 @@ import numpy as np
 import theano
 import theano.tensor as TT
 from lasagne.updates import adam
+
+from rllab.algos.base import RLAlgorithm
+
+class DQN(RLAlgorithm):
+    """
+    Deep Q Network
+    """
+
+    def __init__(
+            self,
+            env,
+            policy,
+            baseline,
+            process_index,
+            scope=None,
+            n_itr=500,
+            start_itr=0,
+            batch_size=5000,
+            max_path_length=500,
+            discount=0.99,
+            gae_lambda=1,
+            plot=False,
+            pause_for_plot=False,
+            center_adv=True,
+            positive_adv=False,
+            store_paths=False,
+            whole_paths=True,
+            fixed_horizon=False,
+            sampler_cls=None,
+            sampler_args=None,
+            force_batch_sampler=False,
+            **kwargs
+    ):
