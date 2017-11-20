b3a28992eca103cab3cb58363dd7a4bb07f250a0
Daniel Marta <daniellsmarta@gmail.com>
2017-11-20 07:31:30 +0000

    Setup fix (#186)
    * setup_fix
    
    * fix setup

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 # setup.py
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
     name='rllab',
+    packages=[package for package in find_packages()
+                if package.startswith('rllab')],
     version='0.1.0',
-    packages=['rllab'],
 )
